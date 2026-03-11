1f9bd0a62d657988dc095c50e0bf30cc9f5d8fb1
VipinKumar-70 <vk20140074250@gmail.com>
2026-03-11 19:23:26 +0530

    feat: implement admin context to fetch detail

--- Frontend/src/api/adminApi.js
+++ Frontend/src/api/adminApi.js
@@ -1,27 +1,14 @@
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 
-export const loginAdmin = async (data) => {
-  const res = await fetch(`${BASE_URL}/admin/login`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
+export const adminProfile = async () => {
+  const res = await fetch(`${BASE_URL}/admin/profile`, {
+    method: "GET",
     credentials: "include",
-    body: JSON.stringify(data),
   });
 
-  return res.json();
-};
-
-export const logoutAdmin = async (data) => {
-  const res = await fetch(`${BASE_URL}/admin/logout`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    credentials: "include",
-    body: JSON.stringify(data),
-  });
+  if (!res.ok) {
+    throw new Error("Not authenticated");
+  }
 
   return res.json();
 };
