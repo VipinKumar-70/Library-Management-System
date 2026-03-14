5b5f475c8d2fff1b8e214fe92d9af27255330ec4
VipinKumar-70 <vk20140074250@gmail.com>
2026-03-14 15:42:58 +0530

    feat: implement Book model

---
+++ Backend/Models/book.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+const bookschema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    // Physical library
+    totalCopies: {
+      type: Number,
+      default: 0,
+    },
+    availableCopies: {
+      type: Number,
+      default: 0,
+    },
+    // Digital library
+    pdfUrl: {
+      type: String,
+      default: null,
+    },
+
+    coverImage: {
+      type: String,
+      default: null,
+    },
+    // Book type
+    bookType: {
+      type: String,
+      enum: ["physical", "digital", "both"],
+      default: "physical",
+    },
+  },
+  { timestaps: true },
+);
+
+module.exports = mongoose.model("Book", bookschema);
