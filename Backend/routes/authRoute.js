c4895d107abbbb82447348d0fdd8aa7dbfe18cb0
VipinKumar-70 <vk20140074250@gmail.com>
2026-03-19 16:05:34 +0530

    feat: secure admin routes and improve authentication logic

--- Backend/routes/authRoute.js
+++ Backend/routes/authRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { register, login, logout } = require("../controllers/userAuth");
+const { register, login, logout } = require("../controllers/authController");
 
 router.post("/register", register);
 
