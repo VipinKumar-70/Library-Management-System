f2435567f351ba09ff5ffd00e0e7220291d13132
VipinKumar-70 <vk20140074250@gmail.com>
2026-03-19 21:09:19 +0530

    feat: implement complete book upload API

--- Backend/server.js
+++ Backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/uploads", express.static("public"));
+app.use("/uploads", express.static("public/uploads"));
 app.use(cookieParser());
 app.use(
   cors({
